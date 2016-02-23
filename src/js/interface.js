df7d59a95fb3a4f2ef446031563f697c3d102d7b
ryspek <ryspek.baybosunov@gmail.com>
2016-02-23 20:51:35 +0300

    finish html/css development of this project

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,8 +1,33 @@
 
-(function($) {
-$(function() {
+(function ($) {
+  $(function() {
 
-  $('input, select, checkbox').styler();
+    $('input, select, input:checkbox').styler();
 
-});
+  });
 })(jQuery);
+
+ $('.list-control-sort__toggle').click(function(e) {
+   e.preventDefault();
+   var self = $(this);
+ 	if (self.hasClass('active')) {
+     self.removeClass('active');
+   } else {
+     self.addClass('active');
+   }
+
+ });
+
+$(document).ready(function() {
+  var tabContainers = $('.big-photo a');
+  tabContainers.hide().filter(':first').show();
+
+  $('.small-photos a').click(function (e) {
+    e.preventDefault();
+    tabContainers.hide();
+    tabContainers.filter(this.hash).fadeIn(300);
+    $('.small-photos a').removeClass('active');
+    $(this).addClass('active');
+    return false;
+  }).filter(':first').click();
+});
